a3f6d92696dc8594c767d68fc3405b5f51a719a9
Ismile Hossain <iamismile.dev@gmail.com>
2023-01-13 00:40:39 +0600

    prevent sql injection

--- src/pool.js
+++ src/pool.js
@@ -12,9 +12,8 @@ class Pool {
     return this._pool.end();
   }
 
-  // REALLY BIG SECURITY ISSUE HERE
-  query(sql) {
-    return this._pool.query(sql);
+  query(sql, params) {
+    return this._pool.query(sql, params);
   }
 }
 
--- src/repos/user-repo.js
+++ src/repos/user-repo.js
@@ -11,9 +11,12 @@ class UserRepo {
   }
 
   static async findById(id) {
-    const { rows } = await pool.query(`
-      SELECT * FROM users WHERE id = ${id};
-    `);
+    const { rows } = await pool.query(
+      `
+      SELECT * FROM users WHERE id = $1;
+    `,
+      [id]
+    );
 
     return toCamelCase(rows)[0];
   }
